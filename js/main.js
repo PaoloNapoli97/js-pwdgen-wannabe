4260d842276c27510f93f1b9604e7de990929ac2
PaoloNapoli97 <paolonapoli@live.it>
2022-10-19 16:03:09 +0200

    Aggiunto esercizio completo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const firstName = prompt("Inserisci il tuo nome");
+const lastName = prompt("Inserisci il tuo cognome");
+const favoriteColor = prompt("Inserisci il tuo colore preferito");
+const password = firstName + lastName + favoriteColor;
+console.log(password);
+
+document.querySelector("h3").innerHTML = password
