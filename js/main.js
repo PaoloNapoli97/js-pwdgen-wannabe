fa8e4be644822cb9d6d284f2aa4f0010831d54e6
PaoloNapoli97 <paolonapoli@live.it>
2022-10-20 09:59:33 +0200

    Fine

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const firstName = prompt("Inserisci il tuo nome");
 const lastName = prompt("Inserisci il tuo cognome");
 const favoriteColor = prompt("Inserisci il tuo colore preferito");
