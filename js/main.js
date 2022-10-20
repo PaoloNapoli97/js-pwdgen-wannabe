50376716ae5ec8fe047a062989cc99f79187ea68
PaoloNapoli97 <paolonapoli@live.it>
2022-10-20 09:58:40 +0200

    Forgot to add "76"

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 const firstName = prompt("Inserisci il tuo nome");
 const lastName = prompt("Inserisci il tuo cognome");
 const favoriteColor = prompt("Inserisci il tuo colore preferito");
-const password = firstName + lastName + favoriteColor;
+const password = firstName + lastName + favoriteColor + 76;
 console.log(password);
 
 document.querySelector("h3").innerHTML = password
